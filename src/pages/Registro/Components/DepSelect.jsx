c9d01e1d29ed272c4a6f75f955c89ec143f1e04c
Yhonny Aplicano <yhonny296@gmail,com>
2023-02-24 15:59:45 -0600

    prototipo de registro

--- src/pages/Registro/Components/DepSelect.jsx
+++ src/pages/Registro/Components/DepSelect.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React from "react";
+import {
+  Form
+} from "react-bootstrap";
+import {AiFillCaretDown} from 'react-icons/ai';
 
 export const DepSelect = () => {
   return (
-    <select>
-    <option value="Atlantida">Atlántida</option>
-    <option value="Choluteca">Choluteca</option>
-    <option value="Colon">Colón</option>
-    <option value="Comayagua">Comayagua</option>
-    <option value="Copan">Copán</option>
-    <option value="Cortes">Cortés</option>
-    <option value="El Paraiso">El Paraíso</option>
-    <option value="Francisco Morazan">Francisco Morazán</option>
-    <option value="Gracias a Dios">Gracias a Dios</option>
-    <option value="Intibuca">Intibucá</option>
-    <option value="Islas de la Bahia">Islas de la Bahía</option>
-    <option value="La Paz">La Paz</option>
-    <option value="Lempira">Lempira</option>
-    <option value="Ocotepeque">Ocotepeque</option>
-    <option value="Olancho">Olancho</option>
-    <option value="Santa Barbara">Santa Bárbara</option>
-    <option value="Valle">Valle</option>
-    <option value="Yoro">Yoro</option>
-  </select> 
-  )
-}
+    <Form.Select aria-label="Deparatamentos" style={{background: '#888C81'}}>
+      <AiFillCaretDown/>
+      <option value="Atlantida">Atlántida</option>
+      <option value="Choluteca">Choluteca</option>
+      <option value="Colon">Colón</option>
+      <option value="Comayagua">Comayagua</option>
+      <option value="Copan">Copán</option>
+      <option value="Cortes">Cortés</option>
+      <option value="El Paraiso">El Paraíso</option>
+      <option value="Francisco Morazan">Francisco Morazán</option>
+      <option value="Gracias a Dios">Gracias a Dios</option>
+      <option value="Intibuca">Intibucá</option>
+      <option value="Islas de la Bahia">Islas de la Bahía</option>
+      <option value="La Paz">La Paz</option>
+      <option value="Lempira">Lempira</option>
+      <option value="Ocotepeque">Ocotepeque</option>
+      <option value="Olancho">Olancho</option>
+      <option value="Santa Barbara">Santa Bárbara</option>
+      <option value="Valle">Valle</option>
+      <option value="Yoro">Yoro</option>
+    </Form.Select>
+  );
+};
--- src/pages/Registro/Registro.jsx
+++ src/pages/Registro/Registro.jsx
@@ -1,29 +1,146 @@
-import {DepSelect  } from "./Components/DepSelect";
+import { DepSelect } from "./Components/DepSelect";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Button,
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import "./style.css";
 
 export const Registro = () => {
-	return (
-		<>
-			<h2>Crea tu cuenta de : </h2>
-			<form>
-				<label>Nombre</label>
-				<input type='text' />
+  return (
+    <>
+      <header className="App-header">
+        <h2 style={{ color: "#59362E" }}>Crea tu cuenta de : </h2>
 
-				<label>Apellido</label>
-				<input type='text' />
+        <Form
+          style={{
+            justifyContent: "space-between",
+            background: "#F2E8DF",
+            width: "600px",
+            height: "850px",
+            margin: "15px auto",
+            padding: "40px",
+            boxShadow: "3px 3px 3px 3px rgba(32,32,32,0.3)",
+          }}
+        >
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicNombre"
+            style={{ color: "#3D403A" }}
+          >
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              style={{
+                background:
+                  "#888c81" /* Falta cambiar el color al placeholder */,
+              }}
+              type="text"
+              placeholder="Ingrese su nombre"
+            />
+          </Form.Group>
 
-				<label>Correo</label>
-				<input type='text' />
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicApellido"
+            style={{ color: "#3D403A" }}
+          >
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control
+              style={{
+                background:
+                  "#888c81" /* Falta cambiar el color al placeholder */,
+              }}
+              type="text"
+              placeholder="Ingrese su apellido"
+            />
+          </Form.Group>
 
-				<label>Contraseña</label>
-				<input type='text' />
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicEmail"
+            style={{ color: "#3D403A" }}
+          >
+            <Form.Label>Correo</Form.Label>
+            <Form.Control
+              style={{
+                background:
+                  "#888c81" /* Falta cambiar el color al placeholder */,
+              }}
+              type="email"
+              placeholder="Ingrese su correo electronico"
+            />
+            <Form.Text className="text-muted" style={{ color: "#3D403A" }}>
+              Nunca compartiremos tu correo electronico.
+            </Form.Text>
+          </Form.Group>
 
-				<label>Repite la contraseña</label>
-				<input type='text' />
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label style={{ color: "#3D403A" }}>Contrasenia</Form.Label>
+            <Form.Control
+              style={{ background: "#888c81" }}
+              type="password"
+              placeholder="Ingrese su contrasenia"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label style={{ color: "#3D403A" }}>
+              Confirmar ontrasenia
+            </Form.Label>
+            <Form.Control
+              style={{ background: "#888c81" }}
+              type="password"
+              placeholder="Ingrese su contrasenia"
+            />
+          </Form.Group>
 
-				<label htmlFor="">Departamentos</label>
-				<DepSelect/>
-				<button>Enviar</button>
-			</form>
-		</>
-	);
+          <DepSelect />
+		  
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check
+              style={{ justifyContent: "space-between", color: "#3D403A" }}
+              type="checkbox"
+              label="Echa un vistazo a los terminos y condiciones"
+            />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="submit"
+            style={{
+              background: "#A67356",
+              border: "#F2E8DF",
+              boxShadow: "5px 5px 1px 1px rgba(32,32,32,0.3)",
+            }}
+          >
+            Enviar
+          </Button>
+        </Form>
+
+        <form>
+          <label>Nombre</label>
+          <input type="text" />
+
+          <label>Apellido</label>
+          <input type="text" />
+
+          <label>Correo</label>
+          <input type="text" />
+
+          <label>Contraseña</label>
+          <input type="text" />
+
+          <label>Repite la contraseña</label>
+          <input type="text" />
+
+          <label htmlFor="">Departamentos</label>
+          <DepSelect />
+          <button>Enviar</button>
+        </form>
+      </header>
+    </>
+  );
 };
