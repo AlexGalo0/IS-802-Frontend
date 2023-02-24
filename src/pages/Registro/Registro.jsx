36fde511870cce161bf6af91e701fbbfbca7d99c
Alex Galo <alexgalo246@gmail.com>
2023-02-24 15:57:08 -0600

    funcionalidad de formulario

--- src/pages/Registro/Registro.jsx
+++ src/pages/Registro/Registro.jsx
@@ -1,27 +1,53 @@
-import {DepSelect  } from "./Components/DepSelect";
-
+import { useForm } from "react-hook-form";
 export const Registro = () => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm();
+
+	const enviarInfo = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<>
 			<h2>Crea tu cuenta de : </h2>
-			<form>
+			<form onSubmit={handleSubmit(enviarInfo)}>
 				<label>Nombre</label>
-				<input type='text' />
+				<input type='text' {...register("nombre")} />
 
 				<label>Apellido</label>
-				<input type='text' />
+				<input type='text' {...register("apellido")} />
 
 				<label>Correo</label>
-				<input type='text' />
+				<input type='text' {...register("email")} />
 
 				<label>Contraseña</label>
-				<input type='text' />
+				<input type='text' {...register("password")} />
 
-				<label>Repite la contraseña</label>
-				<input type='text' />
+				<label htmlFor=''>Departamentos</label>
+				<select {...register("departamento")}>
+					<option value='Atlantida'>Atlántida</option>
+					<option value='Choluteca'>Choluteca</option>
+					<option value='Colon'>Colón</option>
+					<option value='Comayagua'>Comayagua</option>
+					<option value='Copan'>Copán</option>
+					<option value='Cortes'>Cortés</option>
+					<option value='El Paraiso'>El Paraíso</option>
+					<option value='Francisco Morazan'>Francisco Morazán</option>
+					<option value='Gracias a Dios'>Gracias a Dios</option>
+					<option value='Intibuca'>Intibucá</option>
+					<option value='Islas de la Bahia'>Islas de la Bahía</option>
+					<option value='La Paz'>La Paz</option>
+					<option value='Lempira'>Lempira</option>
+					<option value='Ocotepeque'>Ocotepeque</option>
+					<option value='Olancho'>Olancho</option>
+					<option value='Santa Barbara'>Santa Bárbara</option>
+					<option value='Valle'>Valle</option>
+					<option value='Yoro'>Yoro</option>
+				</select>
 
-				<label htmlFor="">Departamentos</label>
-				<DepSelect/>
 				<button>Enviar</button>
 			</form>
 		</>
