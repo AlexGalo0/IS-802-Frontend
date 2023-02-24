fc5b3cf2e0dbd9c8923d9d1af1ebad37e11adb1c
Alex Galo <alexgalo246@gmail.com>
2023-02-23 19:20:51 -0600

    creacion de etiquetas en pagina registro

---
+++ src/pages/Registro/Components/DepSelect.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const DepSelect = () => {
+  return (
+    <select>
+    <option value="Atlantida">Atlántida</option>
+    <option value="Choluteca">Choluteca</option>
+    <option value="Colon">Colón</option>
+    <option value="Comayagua">Comayagua</option>
+    <option value="Copan">Copán</option>
+    <option value="Cortes">Cortés</option>
+    <option value="El Paraiso">El Paraíso</option>
+    <option value="Francisco Morazan">Francisco Morazán</option>
+    <option value="Gracias a Dios">Gracias a Dios</option>
+    <option value="Intibuca">Intibucá</option>
+    <option value="Islas de la Bahia">Islas de la Bahía</option>
+    <option value="La Paz">La Paz</option>
+    <option value="Lempira">Lempira</option>
+    <option value="Ocotepeque">Ocotepeque</option>
+    <option value="Olancho">Olancho</option>
+    <option value="Santa Barbara">Santa Bárbara</option>
+    <option value="Valle">Valle</option>
+    <option value="Yoro">Yoro</option>
+  </select> 
+  )
+}
