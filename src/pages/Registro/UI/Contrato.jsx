860955c4b4548e85ad2844cf65536d2d6d517b6f
Alex Galo <alexgalo246@gmail.com>
2023-02-26 17:19:15 -0600

    correcion contrato

--- src/pages/Registro/UI/Contrato.jsx
+++ src/pages/Registro/UI/Contrato.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export const Contrato = () => {
   return (
-    <>
+    <div>
       <p style={{ textAlign: "justify" }}>
         Bienvenido a 504Marketplace.
         Estos términos y condiciones rigen su acceso y uso del marketplace.
-        <p></p>
-
+        </p>
+        <hr />
         <p>Al acceder o utilizar el marketplace, usted acepta estos términos y
         condiciones y cualquier política que se le proporcione. Si no está de
         acuerdo con estos términos y condiciones, no debe utilizar él
         marketplace. </p>
 
-        <p></p>
+        <hr />
         <p>Para utilizar el marketplace, deberá registrarse y
         crear una cuenta. Usted acepta proporcionar información precisa,
         actualizada y completa durante el proceso de registro. Es su
@@ -24,24 +24,24 @@ export const Contrato = () => {
         los derechos de terceros. Usted acepta no publicar contenido ilegal,
         difamatorio, obsceno, ofensivo, discriminatorio, amenazante o que viole
         los derechos de propiedad intelectual de terceros. </p>
-        <p></p>
+        <hr />
         
         <p>Compras y Ventas: El marketplace permite a los usuarios comprar y vender bienes y servicios.
         Usted acepta que es su responsabilidad determinar la idoneidad de
         cualquier producto o servicio antes de realizar una compra o venta. Él
         marketplace no es responsable de ninguna transacción entre los usuarios.</p>
-        <p></p>
+        <hr />
 
         <p>Pago: El marketplace no procesa los pagos de los usuarios. Cada vendedor
         es responsable de establecer su propia forma de pago y las condiciones
         de venta de sus productos o servicios.</p>
-        <p></p>
+        <hr />
 
         <p>Envío y Entrega: Cada vendedor es
         responsable de establecer sus propios términos y condiciones de envío y
         entrega. El marketplace no es responsable del envío y entrega de los
         productos vendidos.</p>
-        <p></p>
+        <hr />
 
         <p>Propiedad Intelectual: El contenido publicado en él
         marketplace, incluyendo, pero no limitado a texto, imágenes, video y
@@ -50,33 +50,33 @@ export const Contrato = () => {
         no tiene permiso para copiar, reproducir o distribuir ningún contenido
         sin el permiso previo por escrito del marketplace o de los propietarios
         de los derechos. </p>
-        <p></p>
+        <hr />
         
         <p>Modificaciones: El marketplace se reserva el derecho de
         modificar estos términos y condiciones en cualquier momento y sin previo
         aviso. Usted acepta que es su responsabilidad revisar regularmente estos
         términos y condiciones para conocer cualquier cambio.</p>
-        <p></p>
+        <hr />
         
         <p>Responsabilidad:
         Usted acepta que el marketplace no será responsable de ninguna pérdida o
         daño que surja del uso o la imposibilidad de usar el marketplace. En
         ningún caso el marketplace será responsable por daños indirectos,
         Términos y Condiciones especiales, incidentales o consecuentes.</p>
-        <p></p>
+        <hr />
 
         <p>Indemnización: Usted acepta indemnizar y mantener indemne al marketplace,
         sus directores, funcionarios, empleados y agentes de cualquier reclamo o
         demanda, incluyendo honorarios de abogados, presentados por terceros
         debido a su uso del marketplace, su violación de estos términos y
         condiciones, o la violación de los derechos de terceros.</p>
-        <p></p>
+        <hr />
         
         <p>Terminación: Él
         marketplace se reserva el derecho de terminar su cuenta y el acceso al
         marketplace en cualquier momento y por cualquier motivo, sin previo
         aviso.</p>
-        <p></p>
+        <hr />
         
         <p>Publicidad: Usted acepta que el marketplace pueda enviarle
         publicidad por correo electrónico. Si no desea recibir publicidad por
@@ -85,11 +85,11 @@ export const Contrato = () => {
         electrónicos publicitarios. Al aceptar estos términos y condiciones,
         usted acepta que el marketplace pueda enviarle publicidad por correo
         electrónico a menos que opte por no recibirla.</p>
-        <p></p>
+        <hr />
         
         <p>Al utilizar él
         marketplace, usted acepta cumplir con estos términos y condiciones.</p>
-      </p>
-    </>
+     
+    </div>
   );
 };
