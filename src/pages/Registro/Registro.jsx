97dbf5b3f0f37bbec548acf32addf167eb11db79
Alex Galo <alexgalo246@gmail.com>
2023-02-25 22:58:37 -0600

    arreglando problema de merge

--- src/pages/Registro/Registro.jsx
+++ src/pages/Registro/Registro.jsx
@@ -7,15 +7,7 @@ import {
 	Nav,
 	Navbar,
 	NavDropdown,
-	Row,
-	Button,
-	Col,
-	Container,
-	Form,
-	Nav,
-	Navbar,
-	NavDropdown,
-	Row,
+	Row
 } from "react-bootstrap";
 import "./style.css";
 import { useForm } from "react-hook-form";
@@ -25,15 +17,10 @@ import { HiOutlineIdentification } from "react-icons/hi";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { GoLocation } from "react-icons/go";
-
-import {  comprobarEdad  } from "./helpers/comprobarEdad";
+import { comprobarEdad } from "./helpers/comprobarEdad";
 import { createUser } from "../../api/sendRequest.api";
+
 export const Registro = () => {
-	const {
-		register,
-		formState: { errors },
-		handleSubmit,
-	} = useForm();
 	const {
 		register,
 		formState: { errors },
@@ -43,39 +30,13 @@ export const Registro = () => {
 	const enviarInfo = async (data) => {
 		try {
 			console.log(data);
-			const response = await createUser(data)
+			const response = await createUser(data);
 			console.log(response);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
-	return (
-		<>
-			<header className='App-header'>
-				<Container className='Container'>
-					<Form
-						fluid='true'
-						className='Form'
-						onSubmit={handleSubmit(enviarInfo)}
-					>
-						<Row>
-							<Col md>
-								<h2>Logo</h2>
-							</Col>
-							<Col md>
-								<Button
-									className='Buttom-back'
-									variant='secondary'
-									type='submit'
-								>
-									Atras
-								</Button>
-							</Col>
-						</Row>
-						<h1 style={{ color: "#0d0d0d", margin: "15px 0px auto" }}>
-							Crea tu cuenta de :{" "}
-						</h1>
 	return (
 		<>
 			<header className='App-header'>
@@ -119,41 +80,41 @@ export const Registro = () => {
 										{...register("nombres", { required: true, maxLength: 10 })}
 									/>
 
-                  {errors.nombre?.type === "required" && (
-                    <p className="FontAlert">El campo Nombre es requerido</p>
-                  )}
-                  {errors.nombre?.type === "maxLength" && (
-                    <p className="FontAlert">
-                      Tienes demasiados caracteres
-                    </p> /* Añadir otro comentario  */
-                  )}
-                </Form.Group>
-              </Col>
-              <Col md>
-                <Form.Group
-                  className="mb-3, FontMedium"
-                  controlId="formBasicApellido"
-                >
-                  <Form.Label>
-                    <CiUser /> Apellido
-                  </Form.Label>
-                  <Form.Control
-                    className="Input"
-                    type="text"
-                    placeholder="Ingrese su apellido"
-                    {...register("apellido", { required: true, maxLength: 10 })}
-                  />
-                  {errors.apellido?.type === "required" && (
-                    <p className="FontAlert">El campo apellido es requerido</p>
-                  )}
-                  {errors.apellido?.type === "maxLength" && (
-                    <p className="FontAlert">
-                      Tienes demasiados caracteres
-                    </p> /* Añadir otro comentario  */
-                  )}
-                </Form.Group>
-              </Col>
-            </Row>
+									{errors.nombre?.type === "required" && (
+										<p className='FontAlert'>El campo Nombre es requerido</p>
+									)}
+									{errors.nombre?.type === "maxLength" && (
+										<p className='FontAlert'>
+											Tienes demasiados caracteres
+										</p> /* Añadir otro comentario  */
+									)}
+								</Form.Group>
+							</Col>
+							<Col md>
+								<Form.Group
+									className='mb-3, FontMedium'
+									controlId='formBasicApellido'
+								>
+									<Form.Label>
+										<CiUser /> Apellido
+									</Form.Label>
+									<Form.Control
+										className='Input'
+										type='text'
+										placeholder='Ingrese su apellido'
+										{...register("apellido", { required: true, maxLength: 10 })}
+									/>
+									{errors.apellido?.type === "required" && (
+										<p className='FontAlert'>El campo apellido es requerido</p>
+									)}
+									{errors.apellido?.type === "maxLength" && (
+										<p className='FontAlert'>
+											Tienes demasiados caracteres
+										</p> /* Añadir otro comentario  */
+									)}
+								</Form.Group>
+							</Col>
+						</Row>
 
 						<Row>
 							<Col md>
@@ -287,64 +248,64 @@ export const Registro = () => {
 								)}
 							</Col>
 
-              <Col md>
-                <Form.Group
-                  className="mb-3, FontMedium"
-                  controlId="formBasicConfirmPassword"
-                >
-                  <Form.Label>
-                    <RiLockPasswordLine />
-                    Confirmar Contraseña
-                  </Form.Label>
-                  <Form.Control
-                    className="Input"
-                    type="password"
-                    placeholder="Ingrese su Contraseña"
-                    {...register("confirmPassword", { required: true })}
-                  />
-                </Form.Group>
-                {errors.password?.type === "required" && (
-                  <p className="FontAlert">El campo password es requerido</p>
-                )}
-                {errors.password?.type === "maxLength" && (
-                  <p className="FontAlert">
-                    Tienes demasiados caracteres
-                  </p> /* Añadir otro comentario  */
-                )}
-              </Col>
-            </Row>
-            <Form.Label className="FontMedium">
-              <GoLocation />
-              Elige tu departamento
-            </Form.Label>
-            <Form.Select
-              className="Input"
-              aria-label="Departamentos"
-              {...register("departamentos", { required: true })}
-            >
-              <option placeholder="Ingrese su nombre"></option>
-              <option value="1">Atlantida</option>
-              <option value="2">Colon</option>
-              <option value="3">Comayagua</option>
-              <option value="4">Copan</option>
-              <option value="5">Cortes</option>
-              <option value="6">Choluteca</option>
-              <option value="7">El Paraíso</option>
-              <option value="8">Francisco Morazán</option>
-              <option value="9">Gracias a Dios</option>
-              <option value="10">Intibucá</option>
-              <option value="11">Islas de la Bahía</option>
-              <option value="12">La Paz</option>
-              <option value="13">Lempira</option>
-              <option value="14">Ocotepeque</option>
-              <option value="15">Olancho</option>
-              <option value="16">Santa Bárbara</option>
-              <option value="17">Valle</option>
-              <option value="18">Yoro</option>
-            </Form.Select>
-            {errors.departamentos?.type === "required" && (
-              <p className="FontAlert">El campo departamentos es requerido</p>
-            )}
+							<Col md>
+								<Form.Group
+									className='mb-3, FontMedium'
+									controlId='formBasicConfirmPassword'
+								>
+									<Form.Label>
+										<RiLockPasswordLine />
+										Confirmar Contraseña
+									</Form.Label>
+									<Form.Control
+										className='Input'
+										type='password'
+										placeholder='Ingrese su Contraseña'
+										{...register("confirmPassword", { required: true })}
+									/>
+								</Form.Group>
+								{errors.password?.type === "required" && (
+									<p className='FontAlert'>El campo password es requerido</p>
+								)}
+								{errors.password?.type === "maxLength" && (
+									<p className='FontAlert'>
+										Tienes demasiados caracteres
+									</p> /* Añadir otro comentario  */
+								)}
+							</Col>
+						</Row>
+						<Form.Label className='FontMedium'>
+							<GoLocation />
+							Elige tu departamento
+						</Form.Label>
+						<Form.Select
+							className='Input'
+							aria-label='Departamentos'
+							{...register("departamentos", { required: true })}
+						>
+							<option placeholder='Ingrese su nombre'></option>
+							<option value='1'>Atlantida</option>
+							<option value='2'>Colon</option>
+							<option value='3'>Comayagua</option>
+							<option value='4'>Copan</option>
+							<option value='5'>Cortes</option>
+							<option value='6'>Choluteca</option>
+							<option value='7'>El Paraíso</option>
+							<option value='8'>Francisco Morazán</option>
+							<option value='9'>Gracias a Dios</option>
+							<option value='10'>Intibucá</option>
+							<option value='11'>Islas de la Bahía</option>
+							<option value='12'>La Paz</option>
+							<option value='13'>Lempira</option>
+							<option value='14'>Ocotepeque</option>
+							<option value='15'>Olancho</option>
+							<option value='16'>Santa Bárbara</option>
+							<option value='17'>Valle</option>
+							<option value='18'>Yoro</option>
+						</Form.Select>
+						{errors.departamentos?.type === "required" && (
+							<p className='FontAlert'>El campo departamentos es requerido</p>
+						)}
 
 						<Form.Group
 							className='mb-3, FontMedium'
