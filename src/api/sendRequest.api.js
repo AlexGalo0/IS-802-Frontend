50aedfea9c2d0e1f6e386b0ccf1cc7e51c9f5940
Alex Galo <alexgalo246@gmail.com>
2023-03-04 14:33:32 -0600

    correccion de nombre y push

--- src/api/sendRequest.api.js
+++ src/api/sendRequest.api.js
@@ -4,6 +4,6 @@ export const createUser = async (userData) => {
     await axios.post("http://localhost:4000/user", userData);
 }
 
-export const login =async (loginData) =>{
+export const iniciarSesion =async (loginData) =>{
     await axios.post('http://localhost:4000/login',loginData) /* Verificar ruta en Backend */
 }
