59b7984e400acab252d1be3a4d9505040f190f30
Alex Galo <alexgalo246@gmail.com>
2023-03-10 00:01:10 -0600

    solucionado error de logueo inicial

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -3,14 +3,20 @@ import { UserContext } from "./UserContext";
 
 export const UserProvider = ({ children }) => {
 	const [userAuth, setUserAuth] = useState(false);
-	useEffect(()=>{
-		const guardarLogin = sessionStorage.getItem("userAuth")
-		setUserAuth(guardarLogin)
-	},[])
+	useEffect(() => {
+		// const guardarLogin = sessionStorage.getItem("userAuth")
+		// setUserAuth(guardarLogin)
+		const guardarLogin = sessionStorage.getItem("userAuth");
+		if (guardarLogin === null) {
+			setUserAuth(false);
+		} else {
+			setUserAuth(guardarLogin)
+		}
+	}, []);
 
-	useEffect(()=>{
-		sessionStorage.setItem("userAuth",userAuth)
-	},[userAuth])
+	useEffect(() => {
+		sessionStorage.setItem("userAuth", userAuth);
+	}, [userAuth]);
 	return (
 		<UserContext.Provider value={{ userAuth, setUserAuth }}>
 			{children}
