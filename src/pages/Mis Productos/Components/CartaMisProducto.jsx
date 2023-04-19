f018a5f93ebd37fae6bdad071b986b2cf294841b
Alex Galo <alexgalo246@gmail.com>
2023-04-19 03:21:29 -0600

    eliminar producto desde usuario

--- src/pages/Mis Productos/Components/CartaMisProducto.jsx
+++ src/pages/Mis Productos/Components/CartaMisProducto.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import {
-  Container,
-  Accordion,
-  Form,
-  Card,
-  Row,
-  Col,
-  OverlayTrigger,
-  Tooltip,
+	Container,
+	Accordion,
+	Form,
+	Card,
+	Row,
+	Col,
+	OverlayTrigger,
+	Tooltip,
+	Modal,
+  Alert,
 } from "react-bootstrap";
 import "../Style/Temp_Principal.css";
 import { Link } from "react-router-dom";
@@ -19,80 +21,132 @@ import io from "socket.io-client";
 
 const socket = io("http://localhost:4000/");
 export const CartaMisProducto = React.forwardRef(({ producto }, ref) => {
-  // const idProducto = producto.id_producto.data.toString();
+	// const idProducto = producto.id_producto.data.toString();
 
-  
-    socket.on("envio-mensaje",()=>{
-      console.log('Desde Mis Productos recibi el mensaje');
-    })
- 
+	socket.on("envio-mensaje", () => {
+		console.log("Desde Mis Productos recibi el mensaje");
+	});
 
-  function scrollToTop() {
-    window.scrollTo(0, 0);
-  }
-  const renderTooltipBorrar = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-      Borrar producto
-    </Tooltip>
-  );
-  const imagen = JSON.parse(producto.imagenes)
-  const postBody = (
-    <>
-      <Col>
-        <Card className="card">
-          <Container className="card-container">
-            <Card.Img variant='top' src={imagen[0]} className='card-image' />
-          </Container>
-          <Card.Body className="card-body">
-            <Card.Title className="card-title">{producto.producto}</Card.Title>
-            <Card.Text className="card-medium">
-            Lps. {producto.precio}
-            </Card.Text>
-            <Card.Text className="card-medium">
-              Categoría : {producto.nombrecategoria}
-            </Card.Text>
-            <Card.Text className="card-medium">
-              Departamento : {producto.departamento}
-            </Card.Text>
-            <Card.Text className="card-medium">
-              Disponibles en inventario: {producto.cantidad}
-            </Card.Text>
-            {/* <div style={{ display: "flex" }}>
-            <Link to={`/producto/${producto.id_producto.data}`} style={{ textDecoration: "none" }}>
-              <button
-                className="buttonProducto"
-                style={{ color: "#f7f7f7", fontSize: "medium" }}
-              >
-                <span className="box">Ver producto</span>
-              </button>
-            </Link>
+	function scrollToTop() {
+		window.scrollTo(0, 0);
+	}
+	const [show, setShow] = useState(false);
+  const [eliminacionCorrecta, setEliminacionCorrecta] = useState(false)
+  const [productoAEliminar,setProductoAEliminar] = useState({})
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	const renderTooltipBorrar = (props) => (
+		<Tooltip id='button-tooltip' {...props}>
+			Borrar producto
+		</Tooltip>
+	);
+	const imagen = JSON.parse(producto.imagenes);
 
-            <OverlayTrigger
-              placement="right"
-              delay={{ show: 250, hide: 400 }}
-              overlay={renderTooltipBorrar}
-            >
-              <button
-                className="buttonProducto"
-                style={{ color: "#f7f7f7", fontSize: "medium", width: "45px" }}
-              >
-                <span className="box">
-                  <AiOutlineDelete style={{ fontSize: "35px" }} />
-                </span>
-              </button>
-            </OverlayTrigger>
-          </div> */}
-          </Card.Body>
-        </Card>
-      </Col>
-    </>
-  );
+	const queryClient = useQueryClient();
+	const borrarProducto = (productoABorrar) => {
+		/* Esta funcion en realidad solo es de prueba , deberia enviarse a la api aqui */
+		console.log(productoABorrar.idProducto.data.toString());
+	};
+	const mutationBorrarProducto = useMutation({
+		mutationFn: (productoAEliminar) => {
+			borrarProducto(productoAEliminar);
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries("misProductos");
+      setEliminacionCorrecta(true)
 
-  const content = ref ? (
-    <article ref={ref}> {postBody}</article>
-  ) : (
-    <article>{postBody}</article>
-  );
+      setTimeout(() => {
+        handleClose()
+        setEliminacionCorrecta(false)
 
-  return content;
+      }, 1000);
+		},
+	});
+
+	const eliminacionProducto = (producto) => {
+		mutationBorrarProducto.mutate(producto);
+	};
+
+	const postBody = (
+		<>
+			<Col>
+				<Card className='card'>
+					<Container className='card-container'>
+						<Card.Img variant='top' src={imagen[0]} className='card-image' />
+					</Container>
+					<Card.Body className='card-body'>
+						<Card.Title className='card-title'>{producto.producto}</Card.Title>
+						<Card.Text className='card-medium'>
+							Lps. {producto.precio}
+						</Card.Text>
+						<Card.Text className='card-medium'>
+							Categoría : {producto.nombrecategoria}
+						</Card.Text>
+						<Card.Text className='card-medium'>
+							Departamento : {producto.departamento}
+						</Card.Text>
+						<Card.Text className='card-medium'>
+							Disponibles en inventario: {producto.cantidad}
+						</Card.Text>
+						<div style={{ display: "flex" }}>
+							<OverlayTrigger
+								placement='right'
+								delay={{ show: 250, hide: 400 }}
+								overlay={renderTooltipBorrar}
+							>
+								<button
+									className='buttonProducto'
+									style={{
+										color: "#f7f7f7",
+										fontSize: "medium",
+										width: "45px",
+									}}
+									onClick={()=>{
+                    setProductoAEliminar(producto)
+                    handleShow()
+                  }}
+								>
+									<span className='box'>
+										<AiOutlineDelete style={{ fontSize: "35px" }} />
+									</span>
+								</button>
+							</OverlayTrigger>
+						</div>
+					</Card.Body>
+				</Card>
+			</Col>
+		</>
+	);
+
+	const content = ref ? (
+		<article ref={ref}> {postBody}</article>
+	) : (
+		<article>{postBody}</article>
+	);
+
+	return (
+		<>
+			{content}
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Seguro que deseas eliminar este producto?</Modal.Title>
+				</Modal.Header>
+				<Modal.Footer>
+					<button variant='primary' onClick={()=>{
+           
+            eliminacionProducto(productoAEliminar)
+          }}>
+						Eliminar Producto
+					</button>
+          {
+            eliminacionCorrecta ? <Alert variant="success">Se elimino correctamente</Alert> : ''
+          }
+					<button variant='secondary' onClick={handleClose}>
+						Cerrar
+					</button>
+				</Modal.Footer>
+			</Modal>
+		</>
+	);
 });
