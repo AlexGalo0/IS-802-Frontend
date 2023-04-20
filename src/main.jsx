1c95ffd599ec7981fb7f9e922445b1ed726cdc35
AlexxFuentes <aafuentes@unah.hn>
2023-04-20 15:52:16 -0600

    Correccion en eliminar de mis productos

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,12 @@ import {
 	QueryClientProvider,
 	
 } from '@tanstack/react-query'
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // Create a client
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<QueryClientProvider client={queryClient}>
-		{/* <ReactQueryDevtools initialIsOpen={false} /> */}
+		<ReactQueryDevtools initialIsOpen={false} />
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
