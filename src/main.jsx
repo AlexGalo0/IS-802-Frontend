afbb726823ff7719aae5d0e58a3d49bc0523dda4
Alex Galo <alexgalo246@gmail.com>
2023-04-20 15:20:25 -0600

    comentando importacion

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {
 	QueryClientProvider,
 	
 } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // Create a client
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
